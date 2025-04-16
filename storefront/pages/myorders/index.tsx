40e98bd9d762eb464fa5e630bae909b5e4857143
dovanminhtai <dotai05678910@gmail.com>
2025-04-16 15:44:10 +0700

    update Ui to show deliveryStatus

--- storefront/pages/myorders/index.tsx
+++ storefront/pages/myorders/index.tsx
@@ -9,9 +9,12 @@ import { getListOrderByCreatedBy, getOrderById } from '@/modules/orders/services
 import { StateOrProvince } from '@/modules/stateorprovince/model/StateOrProvince';
 import { error } from 'console';
 import React, { useEffect, useState } from 'react'
-import { CheckCircle, Loader, Truck, XCircle} from 'lucide-react'
+import { CheckCircle, Loader, Truck, XCircle } from 'lucide-react'
 
-const deliveryStatusTranslations = {
+type DeliveryStatusType = "PREPARING" | "DELIVERING" | "DELIVERED" | "CANCELLED";
+
+
+const deliveryStatusTranslations: Record<DeliveryStatusType, string> = {
   PREPARING: "Đang chuẩn bị",
   DELIVERING: "Đang giao hàng",
   DELIVERED: "Đã giao hàng",
@@ -36,6 +39,7 @@ export default function myorders() {
   const [isDisplay, setisDisplay] = useState<Boolean>(false);
   const [orderModalVm, setOrderModalVm] = useState<OrderVm>();
   const [orderItemModalVm, setOrderItemModalVm] = useState<OrderItemVm[]>();
+  const [renderedStatuses, SetRenderedStatuses] = useState<React.ReactNode[]>();
 
   useEffect(() => {
     getListOrderByCreatedBy()
@@ -83,80 +87,92 @@ export default function myorders() {
     }).catch((error) => console.error(error));
   }
   // Define the steps => Render Steps with active Check => use it inside the order render
-  
 
 
+  useEffect(() => {
+    if (orderModalVm?.orderStatus) {
+      const statusArray: DeliveryStatusType[] = ["PREPARING", "DELIVERING", "DELIVERED", "CANCELLED"];
+      const rendered = statusArray.map((status, index) => {
+        console.log('chekc',status);
+        console.log('checkMODALvM',orderModalVm.orderStatus)
+        
+        const isActive = status === orderModalVm.deliveryStatus;
+        return (
+          <div key={index} className=" items-center mb-3">
+            <div className={`${isActive ? "text-blue-500" : "text-gray-500"} mr-2`}>
+              {deliveryStatusIcon[status]}
+            </div>
+            <span className={`${isActive ? "font-bold text-blue-500" : "text-gray-500"}`}>
+              {deliveryStatusTranslations[status]}
+            </span>
+          </div>
+        )
+      });
+      SetRenderedStatuses(rendered);
+    }
+  }, [orderModalVm?.orderStatus])
   return (
     <>
-      <div className="container mx-auto">
-        <h2 className="text-center font-bold">Danh sách đơn hàng</h2>
-        {orderVm && orderVm.map((item, index) => (
-          <div key={index}>
-            <div className="flex flex-col w-[70%] mx-auto mt-5 gap-3" onClick={() => setisDisplay(!isDisplay)}>
+      <div className="container flex w-full mx-auto">
+        <div className="w-[80%]">
+          <h2 className="text-center font-bold">Danh sách đơn hàng</h2>
+          {orderVm && orderVm.map((item, index) => (
+            <div key={index}>
+              <div className="flex flex-col w-[70%] mx-auto mt-5 gap-3" onClick={() => setisDisplay(!isDisplay)}>
 
-              <>
-                <div className="mx-auto"
-                  onClick={() => handleModalOrderDetail(item.id)}
-                >
-                  <div className="flex justify-between">
-                    <div>
-                      ID Đơn hàng: {item.id}
+                <>
+                  <div className="mx-auto"
+                    onClick={() => handleModalOrderDetail(item.id)}
+                  >
+                    <div className="flex justify-between">
+                      <div>
+                        ID Đơn hàng: {item.id}
+                      </div>
+                      <div>Trạng thái đơn hàng : {deliveryStatusTranslations[item.deliveryStatus]}</div>
                     </div>
-                    <div>Trạng thái đơn hàng : {deliveryStatusTranslations[item.deliveryStatus]}</div>
-                  </div>
-                  <div>Địa chỉ giao hàng : {item.shippingAddressVm.addressLine1}, {item.shippingAddressVm.addressLine2},
-                    {getStateOrProvincesName(item.shippingAddressVm.stateOrProvinceId)},
-                    {getDistrictsName(item.shippingAddressVm.districtId)}
+                    <div>Địa chỉ giao hàng : {item.shippingAddressVm.addressLine1}, {item.shippingAddressVm.addressLine2},
+                      {getStateOrProvincesName(item.shippingAddressVm.stateOrProvinceId)},
+                      {getDistrictsName(item.shippingAddressVm.districtId)}
 
-                  </div>
-                  <div>Địa chỉ thanh toán : {item.billingAddressVm.addressLine1}, {item.billingAddressVm.addressLine2},
-                    {getStateOrProvincesName(item.billingAddressVm.stateOrProvinceId)},
-                    {getDistrictsName(item.billingAddressVm.districtId)}
-
-                  </div>
-                  {Array.from(item.orderItemVms).map((item, index) => (
-                    <div key={index}>
-                      <>
-                        {item.productId}
-                      </>
                     </div>
-                  ))}
-                </div>
-                <div>
-                </div >
-              </>
+                    <div>Địa chỉ thanh toán : {item.billingAddressVm.addressLine1}, {item.billingAddressVm.addressLine2},
+                      {getStateOrProvincesName(item.billingAddressVm.stateOrProvinceId)},
+                      {getDistrictsName(item.billingAddressVm.districtId)}
 
+                    </div>
+                    {Array.from(item.orderItemVms).map((item, index) => (
+                      <div key={index}>
+                        <>
+                          {item.productId}
+                        </>
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                  </div >
+                </>
+
+
+              </div>
 
             </div>
-
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="w-[20%]">
+          123
+        </div>
 
         {isDisplay && (
           <>
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center" onClick={() => setisDisplay(!isDisplay)}>
-              <div className="bg-white w-96 p-6 rounded-lg shadow-lg relative">
-               {orderModalVm &&  (
-                <div>
-                  {orderModalVm.deliveryStatus === 'PREPARING'}
-                
-                <div>{deliveryStatusIcon[orderModalVm.deliveryStatus]}</div>
-                <div>{orderModalVm.deliveryStatus === 'PREPARING'}</div>
-                
-                
+              <div className="bg-white w-auto p-6 rounded-lg shadow-lg relative">
+                <div className="text-center">
+                  <h3 className="font-bold mb-4 ">Chi tiết đơn hàng</h3>
+                  <div className="flex gap-3">
+                    {renderedStatuses}
+                  </div>
                 </div>
 
-
-               )}
-               
-                {orderModalVm ? (
-                  <div>
-                    
-                  </div>
-                ) : (
-                  <p>Có lỗi với đơn hàng</p>
-                )}
-              
               </div>
             </div>
           </>
